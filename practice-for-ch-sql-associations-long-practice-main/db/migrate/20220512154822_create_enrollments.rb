15438df367506e92d8229394c09302fd0e69a930
Tianshu Xiao <tx2@g.ucla.edu>
2022-07-14 15:08:25 -0700

    part1 done

---
+++ practice-for-ch-sql-associations-long-practice-main/db/migrate/20220512154822_create_enrollments.rb
@@ -0,0 +1,9 @@
+class CreateEnrollments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :enrollments do |t|
+      t.bigint :course_id, null: false, index: true
+      t.bigint :student_id, null: false, index: true
+      t.timestamps
+    end
+  end
+end
